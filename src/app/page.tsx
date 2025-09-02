e7f699ac3c0f6414076cdaa978ad5188aeeed4f3
Arham Ajaz <arhamajaz18@gmail.com>
2025-09-02 09:51:37 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <LoginPage>
    
    with

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-background p-4">
+    <div className="flex min-h-screen w-full items-center justify-center bg-background p-4">
       <Card className="w-full max-w-sm">
         <CardHeader className="text-center">
           <div className="mb-4 flex justify-center">
